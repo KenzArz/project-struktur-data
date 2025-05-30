d9788fece7ed34ea17583e38e1bcf4c9594b74ed
KenzArz <kenzart05@gmail.com>
2025-05-31 01:49:30 +0700

    add: add movie, list movie, cleanup

--- main.cpp
+++ main.cpp
@@ -14,21 +14,20 @@ struct Movie {
 	string sutradara;
 	string ratingUsia;
 	int duration;
-	Movie *next, *prev;
-	Time *headTime;
-	Time *tailTime; 
+	int dayIndex[7] = {-1, -1, -1, -1, -1, -1, -1};
+	Movie *next;
+	Time time[10];
 };
 
 struct Schedule {
 	string day;
-	int dayIndex;
-	Movie *headMovie;
-	Movie *tailMovie;
 };
 
 struct Bioskop {
 	string bioskop;
-	Schedule schedule[7] = {{"Senin"}, {"Selasa"}, {"Rabu"}, {"Kamis"}, {"Jumat"}, {"Sabtu"}, {"Minggu"}};
+	Movie *headMovie = NULL;
+	Movie *tailMovie = NULL;
+	string schedule[7] = {"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"};
 };
 
 struct Ticket {
@@ -48,46 +47,87 @@ struct User {
 
 User user[6] = {{"Adelyn"}, {"Aldi"}, {"Bajang"}, {"Devika"}, {"Ibnu"}, {"Mediana"}};
 Bioskop bioskop[3] = {{ "FESTIVE WALK CGV" },{ "TECHONMART CGV" }, { "MAL KARAWANG XXI"	}};
-int currentBioskopIndex;
-int currentDayIndex;
-int currentUserIndex;
+Movie* currentMovie;
+int currentBioskopIndex = -1;
+int currentDayIndex = -1;
+int currentUserIndex = -1;
 
 //	TODO:
-//= 0. nyari nama user
-//= 1. Menampilkan daftar bioskop
-//= 2. Menampilkan list hari
-//= 3. Menampilkan list film berdasarkan hari yang sudah dipilih user, 
-//= 4. Menampilkan detail film berdasarkan pilihan user;
-//= 5. Menampilkan jam tayang dari film yang udah dipilih
-//=	6. Membuat fungsi untuk booking kursi
-//= 7. Booking tiket berdasarkan nama movie, jam tayang, dan bioskop yang sudah dimasukin user, ticket id bisa pake alamat memori
-//= 8. Menampilkan semua ticket yang sudah dibeli user
-//= 9. Cari film berdasarkan genre
+//= 1. Menampilkan daftar bioskop -- Mediana
+//= 2. Menampilkan list hari -- Devika
+//= 3. Menampilkan list film berdasarkan hari yang sudah dipilih user -- Aldi,
+//= 4. Menampilkan detail film berdasarkan pilihan user -- Nata;
+//= 5. Menampilkan jam tayang dari film yang udah dipilih -- Nata
+//=	6. Membuat fungsi untuk booking kursi --  Bajang
+//= 7. Nyimpen tiket berdasarkan nama movie, jam tayang, dan bioskop yang sudah dimasukin user, ticket id bisa pake alamat memori -- Bajang
+//= 8. Menampilkan semua ticket yang sudah dibeli user -- Adelyn
+//= 9. Cari film berdasarkan genre --Aldi
 
 //! Taro file di sini
 #include "addMovie.txt"
 #include "searchUsername.txt"
 #include "tampilkanHari.txt"
 #include "nampilinBioskop.txt"
+#include "displayMovie.txt"
+#include "cleanup.txt"
 
 //! Bikin function percobaan di sini, habis tuh hapus pindahin ke file
 
 
 void head() {
-	cin.ignore().get();
-	cout << "\t\t==============================================" << endl;
-	cout << "\t\t                 FATIXID" << endl;
-	cout << "\t\t==============================================" << endl << endl;
+	system("clear");
+	cout << "\t\t=============================================================" << endl;
+	cout << "\t\t                 	  FATIXID" << endl;
+	cout << "\t\t=============================================================" << endl;
 }
 
 int main() {
-	// head();
-	// cout << "\t\t1. Bioskop";
-	// cout << "\t\t2. Cari movie berdasarkan genre" << endl;
-	// cout << "\t\t3. Cek tiketku" << endl;
-	// cout << "\t\t4. Log Out" << endl;
-	// cout << "\t\t==============================================\n";
-	// cout << "Masukkan pilihan: " << endl;
+	generateMovie();
+	int choice;
+	while (choice != 5) {
+		if(currentUserIndex == -1) {
+			head();
+			userName();
+		}
 
+		head();
+		cout << "\t\t                 	 MENU UTAMA" << endl;
+		cout << "\t\t1. Pilih Bioskop" << endl;
+		cout << "\t\t2. Cek Tiket" << endl;
+		cout << "\t\t3. Cari Movie berdasarkan genre" << endl;
+		cout << "\t\t4. Log Out" << endl;
+		cout << "\t\t5. Akhiri program" << endl;
+		cout << "\t\t=============================================================" << endl;
+		cout << "\t\tMasukkan pilihan anda: ";
+		cin >> choice;
+
+		head();
+		switch (choice) {
+		case 1:
+			nampilinBioskop(currentBioskopIndex);
+			head();
+			tampilkanHari();
+			head();
+			currentMovie = listMovie();
+
+			break;
+		case 2:
+			//CEK TIKET
+			break;
+		case 3:
+			// Cari Genre
+			break;
+		case 4:
+			currentUserIndex = -1;
+			//Log out
+			break;
+		default:
+			cout << "\t\t=============================================================" << endl;
+			cout << "\t\tMenu yang anda pilih tidak tersedia" << endl;
+			break;
+		}
+	}
+	
+	cleanup();
 	return 0;
 }
